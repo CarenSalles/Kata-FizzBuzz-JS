9defd950d590c167000c54ae2b60e0073694fdc9
Caren Salles <carencrisbsalles@gmail.com>
2022-11-22 19:44:08 +0100

    js tests done

---
+++ src/scripts/FizzBuzz.js
@@ -0,0 +1,9 @@
+export function Fizzbuzz(numb) 
+{
+    if(numb % 3 == 0){
+        return "Fizz";
+    }
+    else if(numb % 5 == 0){
+        return "Buzz";
+    }
+}
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,5 +1,27 @@
+import { Fizzbuzz } from "../src/scripts/FizzBuzz";
+
 describe('FizzBuzz', () => {
-    test('should return Fizz', () => {
-        
+    test('should return Fizz if divisible by three', ()  => {
+      //Given
+      let numb = 3;
+      //when
+      const result = Fizzbuzz(numb);
+      //Then
+      expect(result).toBe("Fizz");
+    });
+    test('should return Buzz if divisible by five', () => {
+        //Given
+        let numb = 5;
+        //when
+        const result = Fizzbuzz(numb);
+        //Then
+        expect(result).toBe("Buzz");
+      });
+    test('should return Fizz if divisible by three and five', () => {
+        //Given
+        letnumb = 15;
+        //when
+        const result = 
+
     });
 });
